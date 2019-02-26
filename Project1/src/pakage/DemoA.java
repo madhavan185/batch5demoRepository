c4607c4008954414aaba5a68c963b4c0a78c74a8
Madhavan <madhavan185@gmail.com>
2019-02-26 21:01:00 +0530

    new file added and modified the previous one

--- Project1/src/pakage/DemoA.java
+++ Project1/src/pakage/DemoA.java
@@ -4,7 +4,7 @@ public class DemoA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new code");
+		System.out.println("new from DemoA");
 
 	}
 
---
+++ Project1/src/pakage/DemoB.java
@@ -0,0 +1,11 @@
+package pakage;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubsy
+		System.out.println("from DemoB");
+
+	}
+
+}
