648702813281578c1b61cf37f49aefa2108739e8
madhavan185 <46966535+madhavan185@users.noreply.github.com>
2019-02-27 06:00:35 +0530

    edited2

--- Project1/src/pakage/DemoB.java
+++ Project1/src/pakage/DemoB.java
@@ -4,7 +4,7 @@ public class DemoB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubsy
-		System.out.println("from DemoB edited");
+		System.out.println("from DemoB edited2");
 
 	}
 
