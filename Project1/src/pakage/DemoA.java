5c136760f694b00c947b1948eaaf03144ea304c1
madhavan185 <46966535+madhavan185@users.noreply.github.com>
2019-02-27 09:29:32 +0530

    changes made in branch2

--- Project1/src/pakage/DemoA.java
+++ Project1/src/pakage/DemoA.java
@@ -4,7 +4,7 @@ public class DemoA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new from DemoA");
+		System.out.println("new from DemoA i edited from branch 2");
 
 	}
 
