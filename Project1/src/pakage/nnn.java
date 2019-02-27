b5ad274bf883e1b9342178a87c7fb5b85d0b9e8a
Madhavan <madhavan185@gmail.com>
2019-02-27 21:55:44 +0530

    nvbvcxcbv

--- Project1/src/pakage/nnn.java
+++ Project1/src/pakage/nnn.java
@@ -1,11 +1,9 @@
 package pakage;
 
-public class Branch1a {
+public class nnn {
 
 	public static void main(String[] args) {
-		
 		// TODO Auto-generated method stub
-		System.out.println("from branch 1a modification");
 
 	}
 
